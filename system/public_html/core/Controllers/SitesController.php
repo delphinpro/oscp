30fb5e792a7997a2609b12eff7e43e087994d3e1
delphinpro <delphinpro@yandex.ru>
2024-04-04 09:33:29 +0400

    Refactor Domains

--- system/public_html/core/Controllers/SitesController.php
+++ system/public_html/core/Controllers/SitesController.php
@@ -13,15 +13,21 @@ use OpenServer\Services\Domains;
 
 class SitesController extends Controller
 {
+    private Domains $domains;
+
+    public function __construct()
+    {
+        $this->domains = Domains::make();
+    }
+
     public function __invoke(): Response
     {
         $settings = readIniFile('config/program.ini');
         $isGroupDomains = $settings['menu']['group_projects_by_tld'] ?? false;
-        $domains = Domains::make();
 
         $sites = $isGroupDomains
-            ? $domains->toGroups()
-            : $domains->toArray();
+            ? $this->domains->toGroups()
+            : $this->domains->toArray();
 
         return Response::json([
             'grouped' => $isGroupDomains,
