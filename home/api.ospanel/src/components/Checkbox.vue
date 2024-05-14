1b88d6d2dbd5f3c4965c52d7cec5c2427bd69b46
delphinpro <delphinpro@yandex.ru>
2024-05-14 13:30:05 +0400

    Refactor Checkbox component

--- home/api.ospanel/src/components/Checkbox.vue
+++ home/api.ospanel/src/components/Checkbox.vue
@@ -9,15 +9,21 @@ export default {
   name: 'Checkbox',
 
   props: {
-    label     : { type: String, default: '' },
-    modelValue: Boolean,
+    label     : String,
+    value     : String,
+    modelValue: [Array, Boolean],
   },
 
   emits: ['update:modelValue'],
 
   computed: {
-    isChecked() {
-      return this.modelValue;
+    model: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
     },
   },
 };
@@ -26,10 +32,11 @@ export default {
 <template>
   <div class="checkbox">
     <label class="checkbox__label">
-      <input :checked="isChecked"
+      <input
+          v-model="model"
+          :value="value"
           class="checkbox__input"
           type="checkbox"
-          @change="$emit('update:modelValue', $event.target.checked)"
       >
       <span class="checkbox__indicator">
         <i class="bi bi-check"></i>
@@ -38,3 +45,40 @@ export default {
     </label>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.checkbox {
+  font-size: 1rem;
+  line-height: 1.2rem;
+  display: flex;
+  user-select: none;
+  &__label {
+    display: flex;
+    gap: 0.5rem;
+  }
+  &__input {
+    display: none;
+  }
+  &__indicator {
+    font-size: 1.5rem;
+    line-height: 1rem;
+    display: flex;
+    align-items: center;
+    flex-shrink: 0;
+    justify-content: center;
+    width: 1.2rem;
+    height: 1.2rem;
+    color: var(--success-color);
+    border: 1px solid var(--hr-color-light);
+    border-radius: var(--radius-half);
+    background: #1f2937;
+    .bi { display: none; }
+  }
+  &__label:hover &__indicator {
+    box-shadow: 0 0 3px rgba(#22c55e, 0.5);
+  }
+  &__input:checked ~ &__indicator {
+    .bi { display: block; }
+  }
+}
+</style>
